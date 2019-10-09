1e9c2f7bb3ceec7e8b90209914525f451bd55598
Christopher Wales <cjwales@gmail.com>
2019-10-09 14:42:19 +0100

    country list added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template lang="html">
-
+  <div>
+    <h1>Countries</h1>
+    <ul>
+      <li v-for="country in countries">{{country.name}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'app',
+  data(){
+    return {
+      countries: [],
+    }
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+  }
 }
 </script>
 
